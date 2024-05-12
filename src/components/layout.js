4cbf77804034f0226393f3f2e0d3925ec3b751a6
onni82 <onni.bucht@me.com>
2024-05-12 04:53:22 +0200

    Updated pages, css and uploaded sitemap

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,25 +4,25 @@ import { Link } from "gatsby"
 import Logo from "../images/logo3.png"
 
 const ListLink = props => (
-  <li className="headerlink">
-    <Link to={props.to} className={props.className}>{props.children}</Link>
-  </li>
+    <li className="headerlink">
+        <Link to={props.to} className={props.className}>{props.children}</Link>
+    </li>
 )
 
 export default function Layout({ pageProp, location, children }) {
-  return (
-    <div id="boxcontent">
-      <img src={Logo} alt="" height="36" className="headerimg" />
-      <ul className="centered bigscreen">
-        <ListLink to="/" activeClassName="active">HOME</ListLink>
-        <ListLink to="/programming/" activeClassName="active">PROGRAMMING &amp; WEB DEV</ListLink>
-        <ListLink to="/education/" activeClassName="active">EDUCATION</ListLink>
-        <ListLink to="/skills/" activeClassName="active">SKILLS &amp; TOOLS</ListLink>
-        <ListLink to="/contact/" activeClassName="active">ABOUT &amp; CONTACT</ListLink>
-      </ul>
-      {children}
-      <br />
-      <div id="footer"><p>Copyright &copy; 2024 <span className="blacktext">Onni August Emanuel Bucht</span></p></div>
-    </div>
-  )
+    return (
+        <div id="boxcontent">
+            <img src={Logo} alt="" height="36" className="headerimg" />
+            <ul className="centered bigscreen">
+                <ListLink to="/" activeClassName="active">Work Experience</ListLink>
+                <ListLink to="/education/" activeClassName="active">Education</ListLink>
+                <ListLink to="/background/" activeClassName="active">Background</ListLink>
+            </ul>
+            {children}
+            <div id="footer">
+                <p>Copyright &copy; 2024 <span className="blacktext">Onni August Emanuel Bucht</span></p>
+                <Link to="/contact/" activeClassName="active">Contact</Link>
+            </div>
+        </div>
+    )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,30 @@
 import React from "react"
-//import { Link } from "gatsby"
 import Header from "../components/header"
 import { Seo } from "../components/seo"
 import Layout from "../components/layout"
+import Hda from "../images/hda.png"
+import Avid from "../images/avid_certified.png"
+import BeLicensed from "../images/belicensed_nopadding.png"
+import IrisHadar from "../images/irishadar.png"
 
 export default function Home() {
-  return (
-    <div id="content">
-      <Layout pageProp="home">
-        <div id="page">
-          <Header headerText="Do You Need A Music Producer, Audio Engineer, Voice Actor, Or Web Developer?" className="centered" />
-          <p>Onni August Emanuel Bucht is the right person for your business.</p>
-          <p>With a Bachelor of Arts degree in Sound and Music Production and training in Video Production, he is perfect for any kind of visual or auditory production.</p>
-          <p>As he has studied foreign languages and cultures, he also has a good understanding of people. This means that any work together with him will go smoothly.</p>
-          <br />
-        </div>
-      </Layout>
-    </div>
-  );
+    return (
+        <Layout pageProp="home">
+            <div id="page">
+                <Header headerText="Work Experience" className="centered" />
+                <img src={IrisHadar} alt="Iris Hadar Komvux" width="55" className="skillLogo" />
+                <h2>Iris Hadar</h2>
+                <p>Nov 2023-Jan 2024 - Municipal Adult Education - Mathematics 2a</p>
+                <p>Stockholm/Södertälje, Sweden (online course)</p>
+                <img src={IrisHadar} alt="Iris Hadar Komvux" width="55" className="skillLogo" />
+                <h2>Iris Hadar</h2>
+                <p>July 2023-Sep 2023 - Municipal Adult Education - Programming 1 (C++)</p>
+                <p>Stockholm/Södertälje, Sweden (online course)</p>
+            </div>
+        </Layout>
+    );
 }
 
 export const Head = () => (
-  <Seo title="Home" />
+    <Seo title="Work Experience" />
 )
